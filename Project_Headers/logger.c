ffd18ca86e62cca777dfced6b19498a825f548fb
Rémi Bèges <remi.beges@gmail.com>
2014-10-21 14:12:03 +0200

    MCU side logger update
    does not compile yet but can now define correct dataframe + new
    functions definition

--- Project_Headers/logger.c
+++ Project_Headers/logger.c
@@ -13,17 +13,20 @@ void init_log()
 {
 	int i;
 	Log.current_index = 0;
+	char default_name[] = {"undefined"};
 	for(i = 0 ; i < 128 ; i++)
 	{
-		Log.variables_size[i] = 0;
-		Log.variables_ptr[i] = 0;
-		Log.rw_rights[i] = 1;//1 means readonly
+		Log.variables[i].size = 0;
+		Log.variables[i].ptr = 0;
+		Log.variables[i].rw_rights = 1;//1 means readonly
+		Log.variables[i].id = i;
+		strcpy(Log.variables[i].name,default_name);
 	}
 		
 }
 
 //Returns 0 if ok, 1 if size exceeds log capacity
-uint8_t add_to_log(log* table, uint8_t* adress, uint16_t octets, uint8_t readonly)
+uint8_t add_to_log(uint8_t* adress, uint16_t octets, datatype type, uint8_t readonly, char* name)
 {
 	if(Log.current_index == 127)
 		return 1;
@@ -32,59 +35,62 @@ uint8_t add_to_log(log* table, uint8_t* adress, uint16_t octets, uint8_t readonl
 	if(octets > 1024)
 		return 1;
 	
-	Log.variables_ptr[Log.current_index] = adress;
-	Log.variables_size[Log.current_index] = octets;
-	Log.rw_rights[Log.current_index] = readonly;
+	Log.variables[Log.current_index].ptr = adress;
+	Log.variables[Log.current_index].size = octets;
+	Log.variables[Log.current_index].rw_rights = readonly;
+	Log.variables[Log.current_index].type = type;
+	strcpy(Log.variables[Log.current_index].name,name);
 	
 	Log.current_index++;
 	
 	return 0;
 }
 
-void update_serial()
+void update_log_serial()
 {
 	int i,j, adress;
 	uint8_t buffer;
 	uint8_t header = 85;//01010101
 	uint8_t footer = 204;//11001100
 	uint8_t *temp_ptr;
+	uint8_t type = 1;
 	
 	//Transmit data to serial
 	for(i = 0 ; i < Log.current_index ; i++)
 	{
 		
-		//Set header (2 octets)
-		serial_write(header,1);
-		serial_write(header,1);
+		//Write header
+		serial_write(&header,1);
 		
-		//Set framesize
-		uint16_t framesize = Log.variables_size[i];
-		temp_ptr = (uint8_t*)(&framesize);
-		serial_write(*temp_ptr,1); 		
-		serial_write(*(temp_ptr+1),1);
+		//Write data size
+		uint16_t datasize = Log.variables[i].size;
+		temp_ptr = (uint8_t*)(&datasize);
+		serial_write(temp_ptr,2); 		
 		
-		//Set CMD
-			//1 is return_log_table..TBD!
-			//2 is return_log_values
-		serial_write(2,1);
-		
-		//Set data
-		for(j = 0 ; j < Log.variables_size[i] ; j++)
+		//Write data type
+		switch(Log.variables[i].type)
 		{
-			adress = i + j;
-			buffer = *(Log.variables_ptr[adress]);
-			serial_write(buffer,1);
+			case FLOAT:
+				type = 1;
+				break;
+			case INT32:
+				type = 2;
+				break;
 		}
-	
+		serial_write(&type,1);
 		
+		//Write variable ID
+		temp_ptr = (uint8_t*)(&datasize);
+		serial_write(temp_ptr,2);
+		
+		//Write command
+		type = 15;//b'0000 1111 - variable value answer
+		serial_write(&type,1);
+		
+		//Write data
+		serial_write(Log.variables[i].ptr,Log.variables[i].size);
+	
 		//Set EOFrame
 		serial_write(footer,1);		
 	}
-		
-	
-	//Read data from serial
-	/*while(serial_available())
-	{
-		
-	}*/
 }
--- Project_Headers/logger.h
+++ Project_Headers/logger.h
@@ -11,12 +11,34 @@
 //1 log can retain 128 different variables (=ptr adress) (A table only counts 1)
 #include "TFC\TFC.h"
 
+typedef enum datatype datatype;
+enum datatype
+{
+	FLOAT,
+	INT32,
+	INT16,
+	INT8,
+	UINT32,
+	UINT16,
+	UINT8
+	
+};
+
+typedef struct variable variable;
+struct variable
+{
+	uint8_t* ptr;
+	uint16_t size;
+	uint8_t rw_rights;
+	uint16_t id;
+	datatype type;
+	char name[32];
+};
+
 typedef struct log log;
 struct log
 {
-	uint8_t* variables_ptr[128];
-	uint16_t variables_size[128];
-	uint8_t rw_rights[128];
+	variable variables[128];
 	uint16_t current_index;
 };
 
@@ -25,8 +47,18 @@ extern log Log;
 void init_log();
 
 //Returns 0 if ok, 1 if size exceeds log capacity
-uint8_t add_to_log(log* table, uint8_t* adress, uint16_t octets, uint8_t readonly);
+uint8_t add_to_log(uint8_t* adress, 
+				   uint16_t octets,
+				   datatype type, 
+				   uint8_t readonly, 
+				   char* name);
 
 void update_log_serial();
+
+//TBD
+void process_serial();
+
+//TBD
+void send_table();
 
 #endif /* LOGGER_H_ */
