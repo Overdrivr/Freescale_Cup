79c50948732f87146959d413af49a48554b46201
Rémi Bèges <remi.beges@gmail.com>
2014-10-23 18:25:34 +0200

    Worker thread for GUI app

---
+++ PythonDebugSources/Debug_app/Worker.py
@@ -0,0 +1,33 @@
+import random
+import sys
+from threading import Thread
+import time
+import serial
+import queue
+from serial.tools.list_ports import comports
+from SerialProtocol import SerialProtocol
+
+#Serial data processing class
+class Worker(Thread):
+
+    def __init__(self,serial_thread):
+        Thread.__init__(self)
+        self.stop_signal = 0
+        self.serial_thread = serial_thread
+        self.serial_protocol = SerialProtocol
+        
+    def stop(self):
+        self.stop_signal = 1;
+
+    def run(self):
+        while self.stop_signal == 0:
+            #If byte has been received
+            if self.serial_thread.available():
+                #Get it
+                byte = self.serial_thread.read()
+                #Then feed it to serial protocol
+                self.serial_protocol.new_rx_byte(byte)
+
+
+                
+        
