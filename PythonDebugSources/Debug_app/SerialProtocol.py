cc5b6d3689a4ee362bd73b4d442ffb2464f3f745
Rémi Bèges <remi.beges@gmail.com>
2014-10-24 15:10:20 +0200

    Fixed serial protocol (Py)

--- PythonDebugSources/Debug_app/SerialProtocol.py
+++ PythonDebugSources/Debug_app/SerialProtocol.py
@@ -3,7 +3,7 @@ import sys
 from threading import Thread
 import time
 import serial
-import queue
+from queue import Queue
 from serial.tools.list_ports import comports
 from enum import Enum
 
@@ -18,21 +18,25 @@ class ESC_STATE(Enum):
 #Robust serial protocol with bit stuffing
 class SerialProtocol():
 
-    def __init__(self):
+    def __init__(self,fullframe_callback):
         self.rx_state = RX_STATE.IDLE;
         self.escape_state = ESC_STATE.IDLE;
         self.SOF = 0x7F
         self.EOF = 0x7F
         self.ESC = 0x7D
         self.frame_queue = Queue(0)
+        self.fullframe_callback = fullframe_callback
 
     def new_rx_byte(self, newbyte):
+        print(newbyte)
+        newbyte = int.from_bytes(newbyte, byteorder='big')
+        print(newbyte)
         #No frame in process
         if self.rx_state == RX_STATE.IDLE:
             if newbyte == self.SOF:
                 # New frame started
                 self.rx_state = RX_STATE.IN_PROCESS
-                
+                print("SOF")
         #Frame is in process        
         else:
             #Next char must be data
@@ -41,17 +45,19 @@ class SerialProtocol():
                 #See serial_protocols_definition.xlsx
                 self.frame_queue.put(newbyte)
                 self.escape_state == ESC_STATE.IDLE
-                print(newbyte)
                 
             #Next char can be data or flag (EOF, SOF,..)    
             elif self.escape_state == ESC_STATE.IDLE:
                 if newbyte == self.EOF:
-                    #Signal EOF ?
+                    self.fullframe_callback(self.frame_queue)
+                    print("EOF")
+                    #Empty queue
+                    self.frame_queue = Queue(0)
                     self.rx_state == RX_STATE.IDLE
                 elif newbyte == self.ESC:
                     self.escape_state == ESC_STATE.NEXT
                 else:
                     self.frame_queue.put(newbyte)
-                    print(newbyte)
+    
         
 
