96c867e4364a0fe66ccd4d376c0b74183d3e8c39
Rémi Bèges <remi.beges@gmail.com>
2014-10-30 18:33:43 +0100

    Logger.py can now decode simple payloads

--- PythonDebugSources/Debug_app/Logger.py
+++ PythonDebugSources/Debug_app/Logger.py
@@ -3,6 +3,7 @@ import sys
 from threading import Thread
 import time
 from queue import Queue
+import struct
 
 #Serial data processing class
 class Logger():
@@ -13,25 +14,39 @@ class Logger():
 
     #Process RX bytes queue
     def new_frame(self,frame):
-        command = frame.get()
-        datatype = frame.get()
-        dataid1 = frame.get()
-        dataid2 = frame.get()
-        dataid = int.from_bytes(dataid1,"big") << 8 + int.from_bytes(dataid2,"big")
+        command = int.from_bytes(frame.get(),"big")
+        datatype = int.from_bytes(frame.get(),"big")
 
-        print("command [",command,"] ; datatype [", datatype,"], dataid [",dataid"]")
+        dataid1 = int.from_bytes(frame.get(),"big")
+        dataid2 = int.from_bytes(frame.get(),"big")
+        dataid = dataid1 << 8 + dataid2
+
+        #print("command [",command,"] ; datatype [", datatype,"], dataid [",dataid,"]")
         
         #Returned variable value
-        if current_byte == bytes(0x00):
-            
+        if command == 0:
+            if datatype == 6:
+                #while not frame.empty():
+                    temp = bytearray()
+                    temp.insert(1,int.from_bytes(frame.get(),"big"))
+                    temp.insert(1,int.from_bytes(frame.get(),"big"))
+                    temp.insert(1,int.from_bytes(frame.get(),"big"))
+                    temp.insert(1,int.from_bytes(frame.get(),"big"))
+
+                    #Transform value to desired format
+                    val = struct.unpack('i',temp)[0]
+
+                    #Set new value to table
+                    print("val = ",val)
+                
         #Returned variable table
-        elif current_byte == bytes(0x02):
+        elif command == 2:
             while not frame.empty():
                 b1 = frame.get()
                 b2 = frame.get()
                 id = b1 << 8 + b2
                 #Read 32 characters
-        else
+        else:
             print("Logger : unknown MCU answer")
 
         pass
