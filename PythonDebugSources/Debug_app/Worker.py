71b7bd735dee99865a3c997071b347a48d289a70
Rémi Bèges <remi.beges@gmail.com>
2014-10-30 12:01:31 +0100

    Worker.py cleanup
    +new function to send command for querying MCU var table

--- PythonDebugSources/Debug_app/Worker.py
+++ PythonDebugSources/Debug_app/Worker.py
@@ -12,11 +12,28 @@ from Logger import Logger
 class Worker(Thread):
 
     def __init__(self,serial_thread):
+        #Init thread
         Thread.__init__(self)
         self.stop_signal = 0
         self.serial_thread = serial_thread
+
+        #Init logger
         self.logger = Logger()
+
+        #Init serial protocol
         self.serial_protocol = SerialProtocol(self.logger.new_frame)
+        time.sleep(1)
+
+    def get_MCU_table:
+        #Get command for querying variable table MCU side
+        cmd = self.logger.get_command_read_variable_table()
+        
+        #Feed command to serial protocol payload processor 
+        frame = self.serial_protocol.process_tx_payload(cmd)
+
+        #Send command
+        serial_thread.write(frame)
+        
         
     def stop(self):
         self.stop_signal = 1;
