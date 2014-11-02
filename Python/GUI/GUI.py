86c7c737b5ec2f928eb9f7ea0f8b5028ebba8950
Rémi Bèges <remi.beges@gmail.com>
2014-11-02 23:19:38 +0100

    Structuring gui

--- Python/GUI/GUI.py
+++ Python/GUI/GUI.py
@@ -17,22 +17,31 @@ from Worker import Worker
 class Application(Tk.Frame):
         
     def __init__(self, **kwargs):
-        self.fenetre = Tk.Tk()
-        Tk.Frame.__init__(self, self.fenetre, width=1, height=1, **kwargs)
-        self.pack(fill=Tk.BOTH)
+        self.root = Tk.Tk()
+        Tk.Frame.__init__(self,self.root,width=640, height=480)
+        self.pack(fill=Tk.BOTH, expand=1)
+        
+        self.frame_com_ports = Tk.Frame(self)
+        self.frame_com_ports.grid(column=0,row=0)
+        #self.frame_com_ports.pack(side="left", fill="both",expand=True)
 
+        self.separator = Tk.Frame(self,height=2, bg="black", bd=1, relief=Tk.RAISED)
+        self.separator.grid(column=0,row=1, pady=5,sticky='EW')
+            
+        self.frame_logger = Tk.Frame(self)
+        self.frame_logger.grid(column=0,row=2)
+       # self.frame_com_ports.pack(side="left", fill="both")
+        
         # Serial port thread
         self.thread_1 = SerialPortHandler()
         # Main thread 
         self.workerthread = Worker(self.thread_1)
-
-        self.grid()
         
         #Widgets
-        self.txt_ports = Tk.Label(self,text="COM PORTS")
+        self.txt_ports = Tk.Label(self.frame_com_ports,text="COM PORTS")
         self.txt_ports.grid(column=0,row=0)
 
-        self.liste = Tk.Listbox(self,height=1)
+        self.liste = Tk.Listbox(self.frame_com_ports,height=1)
         self.liste.grid(column=0,row=1,sticky='EW')
 
         self.scrollbar_liste = Tk.Scrollbar(self.liste)
@@ -40,20 +49,22 @@ class Application(Tk.Frame):
         self.liste.config(yscrollcommand = self.scrollbar_liste.set)
         self.scrollbar_liste.pack(side=Tk.RIGHT)
         
-        self.bouton_refresh_ports = Tk.Button(self, text="REFRESH", command = self.read_ports)
+        self.bouton_refresh_ports = Tk.Button(self.frame_com_ports, text="REFRESH", command = self.read_ports)
         self.bouton_refresh_ports.grid(column=0,row=2,sticky='EW')
 
-        self.bouton_connect = Tk.Button(self, text="CONNECT", command = self.start_com)
+        self.bouton_connect = Tk.Button(self.frame_com_ports, text="CONNECT", command = self.start_com)
         self.bouton_connect.grid(column=0,row=3,sticky='EW')
         
         self.bouton_quitter = Tk.Button(self, text="x", relief=Tk.GROOVE,command = self.stop)
         self.bouton_quitter.grid(column=1,row=0,sticky='EW')
 
-        self.txt_connected = Tk.Label(self,text="NOT CONNECTED", fg='red', width = 20)
+        self.txt_connected = Tk.Label(self.frame_com_ports,text="NOT CONNECTED", fg='red', width = 20)
         self.txt_connected.grid(column=0,row=4,sticky='EW')
 
-        self.txt_connected = Tk.Label(self,text="LOGGER")
-        self.txt_connected.grid(column=0,row=6,sticky='EW')    
+        self.txt_log = Tk.Label(self.frame_logger,text="LOGGER")
+        self.txt_log.grid(column=0,row=6,sticky='EW')
+
+        
         
         """self.f = Figure(figsize=(4,3), dpi=100)
         self.a = self.f.add_subplot(111)
@@ -65,7 +76,7 @@ class Application(Tk.Frame):
         self.canvas = FigureCanvasTkAgg(self.f, master=fenetre)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-        """         
+        """      
     def read_ports(self):
         ports_list = self.thread_1.get_ports()
         for p, desc, hwid in sorted(ports_list):
