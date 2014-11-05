0ebcf12352a359b869861dbb0fd75490fea5c216
Rémi Bèges <remi.beges@gmail.com>
2014-11-05 15:39:43 +0100

    New module import
    +cleanup

--- Python/GUI/Model.py
+++ Python/GUI/Model.py
@@ -1,20 +1,5 @@
-#import matplotlib
-#matplotlib.use('TkAgg')
-
-#from numpy import arange, sin, pi
-#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-
-import tkinter as Tk
-import random
-import sys
-from threading import Thread
-import time
-
-#from matplotlib.figure import Figure
-from SerialPortHandler import SerialPortHandler
 from Worker import SerialWorker
-from Frames import *
-
+from pubsub import pub
 
 # Top-level API for the GUI
 class Model():
