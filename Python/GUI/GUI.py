a132392ab7a47a82148640466d7caae22aaffba3
Rémi Bèges <remi.beges@gmail.com>
2014-11-12 16:52:56 +0100

    Added test functions

--- Python/GUI/GUI.py
+++ Python/GUI/GUI.py
@@ -3,10 +3,10 @@ import random
 import sys
 from threading import Thread
 import time
-
-#from matplotlib.figure import Figure
 from Frames import *
 from Model import Model
+from threading import Timer
+from pubsub import pub
 
 class Application(Tk.Frame):
         
@@ -30,7 +30,7 @@ class Application(Tk.Frame):
         self.frame_logger.grid(column=0,row=1,sticky='N',pady=5,padx=5)
 
         #Graph frame
-        self.frame_graph1 = Graph_Frame(self,self.model,bd=2,relief=Tk.GROOVE)
+        self.frame_graph1 = Graph_Frame(self,self.model,self.root,bd=2,relief=Tk.GROOVE)
         self.frame_graph1.grid(column=1,row=0,sticky='EW',pady=5,padx=5,rowspan=2)
 
         #Quit button
@@ -39,8 +39,25 @@ class Application(Tk.Frame):
 
     def stop(self):
         self.model.stop()
-        
 
+"""
+Test functions
+"""
+def test_new_log_value():
+    x = 0;
+    print("logger test started.")
+    for i in range(0, 256):
+        test = list()
+        value = sin(x)
+        test.append(value)
+        x += 0.05
+        pub.sendMessage('var_value_update',varid=0,value=test)
+
+"""
+Program startup
+"""
 if __name__ == '__main__':
     app = Application()
+    t = Timer(1.0,test_new_log_value)
+    t.start()
     app.mainloop()
