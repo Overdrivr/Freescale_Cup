5da3ee355f1a5823ead4294f4a270826cb61943c
Rémi Bèges <remi.beges@gmail.com>
2014-11-27 14:31:59 +0100

    Added timeout when joining thread to avoid freeze
    Solving #3 but actual root cause not found clearly. Maybe related to
    using pub.SendMessage in a thread

--- Python/GUI/Model.py
+++ Python/GUI/Model.py
@@ -36,9 +36,11 @@ class Model():
         self.serialthread.stop()
 
         if self.serialthread.isAlive():
-            self.serialthread.join()
-        
-        print("--- All threads stopped.")
+            self.serialthread.join(1)
+            if self.serialthread.isAlive():
+                print("--- Thread not properly joined.")
+            else:
+                print("--- Thread stopped.")
         
     def start_logger(self):
         #Get command for querying variable table MCU side
