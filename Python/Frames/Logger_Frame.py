03cc3f62d0040f96377e36feb18da479962e2216
Rémi Bèges <remi.beges@gmail.com>
2014-12-08 14:22:41 +0100

    Logger frame can now send correct id to write to

--- Python/Frames/Logger_Frame.py
+++ Python/Frames/Logger_Frame.py
@@ -75,9 +75,13 @@ class Logger_Frame(Tk.Frame):
         
         if len(item) == 0:
             return
-
+        
+        #Process tuple from selection() to index
+        #TOCHECK : WHAT HAPPENS WHEN NUMBER > 9
+        id = int(item[0][-1]) - 1
+        
         # Get entry value
         value = self.value.get()
 
         # Tell API to write value
-        self.model.write_to_var(item[0],value)
+        self.model.write_to_var(id,value)
