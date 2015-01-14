26186660fc9eec67ec4736c9f24ce89f4f1f06c2
Rémi Bèges <remi.beges@gmail.com>
2015-01-14 13:20:51 +0100

    Updated main with unit tests

--- Sources/main.c
+++ Sources/main.c
@@ -9,8 +9,22 @@
 
 // WARNING : SysTick frequency is 50kHz. Will overflow after roughly 2.5 hours
 
+void cam_program();
+void test_serial1();
+void test_protocol1();
+void test_distantio_minimal();
+
 int main(void)
 
+{
+	//test_serial1();
+	//cam_program();
+	//test_protocol1();
+	test_distantio_minimal();
+	return 0;
+}
+
+void cam_program()
 {
 	TFC_Init();
 	
@@ -29,9 +43,8 @@ int main(void)
 	float commandD = 0.f;
 	float commandP = 0.f;
 	
-	/*********************/
-	/**      PID        **/
-	/*********************/
+	//      PID        
+	
 	//STABLE
 	float P = 0.012;
 	float D = 0.f;
@@ -108,10 +121,6 @@ int main(void)
 	
 	register_array(data.filtered_image,128,FLOAT,0,"filtered_line");
 	register_array(data.raw_image,128,UINT16,0,"raw_line");
-	
-	/*TICKERS
-	0 : chronos	
-	*/
 	
 	float exposure_time_us = 10000;
 	uint32_t servo_update_ms = 10;
@@ -217,6 +226,136 @@ int main(void)
 		TFC_SetBatteryLED_Level(led_state);
 		Capture(&chr_loop_m);	//**TIME MONITORING
 	}
+}
+
+
+void test_serial1()
+{
+	TFC_Init();
+	
+	chrono chr;
+	
+	uint8_t  testESC[10];
+	testESC[0] = 0x00;
+	testESC[1] = 0x01;
+	testESC[2] = 0x02;
+	testESC[3] = 0x03;
+	testESC[4] = 0x04;
+	testESC[5] = 0x05;
+	testESC[6] = 0x06;
+	testESC[7] = 0x07;
+	testESC[8] = 0x08;
+	testESC[9] = 0x09;
+		
+	init_serial();
+	Restart(&chr);
+	
+	for(;;)
+	{
+		//TFC_Task must be called in your main loop.  This keeps certain processing happy (I.E. Serial port queue check)
+		TFC_Task();
+		
+		Capture(&chr);
+		if(GetLastDelay_us(&chr) > 1500)
+		{
+			Restart(&chr);
+			serial_write(testESC,10);
+		}			
+		
+	}
 	return 0;
 }
 
+void test_protocol1()
+
+{
+	TFC_Init();
+	
+	
+	chrono chr;
+	
+	
+	// data sent : 0x007F7DF7;
+	//For testing all ESC characters
+	
+	uint8_t  testESC[13];
+	testESC[0] = 0xf7;
+	testESC[1] = 0x00;
+	testESC[2] = 0x03;
+	testESC[3] = 0x00;
+	testESC[4] = 0x00;
+		testESC[5] = 0x7D;
+	testESC[6] = 0xf7;
+		testESC[7] = 0x7D;
+	testESC[8] = 0x7D;
+		testESC[9] = 0x7D;
+	testESC[10] = 0x7f;
+	testESC[11] = 0x00;
+	testESC[12] = 0x7f;
+	/*
+	uint8_t  testESC[10];
+	testESC[0] = 0xf7;
+	testESC[1] = 0x00;
+	testESC[2] = 0x03;
+	testESC[3] = 0x00;
+	testESC[4] = 0x00;
+	testESC[5] = 0x00;
+	testESC[6] = 0x00;
+	testESC[7] = 0x00;
+	testESC[8] = 0x00;
+	testESC[9] = 0x7f;
+	*/
+		
+	init_serial();
+	Restart(&chr);
+	
+	for(;;)
+	{
+		//TFC_Task must be called in your main loop.  This keeps certain processing happy (I.E. Serial port queue check)
+		TFC_Task();
+		
+		Capture(&chr);
+		if(GetLastDelay_us(&chr) > 1500)
+		{
+			Restart(&chr);
+			serial_write(testESC,13);
+		}			
+		
+	}
+	return 0;
+}
+
+void test_distantio_minimal()
+{
+	TFC_Init();
+	
+	
+	init_serial();
+	init_serial_protocol();
+	init_log();
+	
+	//TFC_HBRIDGE_ENABLE;
+	
+	uint32_t testESC =  0x007F7DF7;
+	
+	//Readonly variables
+	register_scalar(&testESC,UINT32,0,"TestESC");
+	
+	chrono chr_distantio;
+	Restart(&chr_distantio);
+	
+	for(;;)
+	{
+			
+		//TFC_Task must be called in your main loop.  This keeps certain processing happy (I.E. Serial port queue check)
+		TFC_Task();
+		
+		Capture(&chr_distantio);
+		if(GetLastDelay_us(&chr_distantio) > 500)
+		{
+			Restart(&chr_distantio);
+				
+			update_distantio();
+		}			
+	}
+}
