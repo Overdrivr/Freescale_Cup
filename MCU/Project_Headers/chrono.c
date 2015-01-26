88e84593ada808465b5bb7354a42a6fc79e03d31
Rémi Bèges <remi.beges@gmail.com>
2015-01-26 18:16:40 +0100

    More accurate option for chronos

--- MCU/Project_Headers/chrono.c
+++ MCU/Project_Headers/chrono.c
@@ -24,3 +24,13 @@ float ms(chrono* chr)
 	chr->stop = TFC_Ticker[0];
 	return (chr->stop - chr->start) * 1000.f / (float)(SYSTICK_FREQUENCY);
 }
+
+void remove_us(chrono* chr, uint32_t delay)
+{
+	chr->start += delay * (float)(SYSTICK_FREQUENCY) / (1000.f * 1000.f); 
+}
+
+void remove_ms(chrono* chr, uint32_t delay)
+{
+	chr->start += delay * (float)(SYSTICK_FREQUENCY) /  1000.f; 
+}
